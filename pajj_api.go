565a98208468a0cde28390cf6c588fa6a0f6cc86
Hadar Greinsmark <hadarg@kth.se>
2017-03-02 21:43:17 +0100

    Begin REST structure.

---
+++ pajj_api.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+	"log"
+	"fmt"
+	"encoding/json"
+)
+
+type authorizeRequest struct {
+	SessionToken string `json:"session_token"`
+}
+
+func main() {
+	print("starting server")
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/authorize", authorize)
+
+	err := http.ListenAndServe(":8118", mux)
+	log.Fatal(err)
+}
+
+func authorize(w http.ResponseWriter, r *http.Request) {
+	authReq := authorizeRequest{}
+	json.Unmarshal(r.Body, &authReq)
+	w.WriteHeader(200)
+
+	fmt.Fprintln(w, "Hello browser")
+}
