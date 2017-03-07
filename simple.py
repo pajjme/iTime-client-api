cef0452664f866364a69fd2bab9cc4745901e546
David Skeppstedt <david.skeppstedt@gmail.com>
2017-03-07 16:51:50 +0100

    Add random existing user response

--- simple.py
+++ simple.py
@@ -5,10 +5,19 @@ from flask_cors import CORS, cross_origin
 app = Flask(__name__)
 CORS(app)
 
+new_user = True
+
 @app.route('/login', methods=['POST'])
 def hello():
+    global new_user
     print(request.data)
-    return "Hi Jonas!\n"
+    if new_user:
+        new_user = False
+        return "New User\n"
+    else:
+        new_user = True
+        return "Existing User\n"
 
 if __name__ == "__main__":
+    new_user = True
     app.run()
