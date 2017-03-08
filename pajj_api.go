6ad51c2b732cb2304352f7945aec8d5d9ef9fe10
Hadar Greinsmark <hadarg@kth.se>
2017-03-08 21:31:09 +0100

    Implement stats endpoint.

--- pajj_api.go
+++ pajj_api.go
@@ -8,18 +8,24 @@ import (
 	"io/ioutil"
 )
 
+const ApiVersion string = "/v1"
+
 type authorizeRequest struct {
 	SessionToken string `json:"session_token"`
 }
 
 func main() {
 	print("starting server")
-	mux := http.NewServeMux()
 
-	mux.HandleFunc("/authorize", authorize)
-
-	err := http.ListenAndServe(":8118", mux)
+	err := http.ListenAndServe(":8118", mainHandler())
 	log.Fatal(err)
+}
+
+func mainHandler() http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc(ApiVersion + "/authorize", authorize)
+	mux.HandleFunc(ApiVersion + "/stats", stats)
+	return mux
 }
 
 func authorize(w http.ResponseWriter, r *http.Request) {
@@ -29,4 +35,19 @@ func authorize(w http.ResponseWriter, r *http.Request) {
 	w.WriteHeader(200)
 
 	fmt.Fprintln(w, "Hello browser")
+}
+
+func stats(w http.ResponseWriter, r *http.Request) {
+	params := r.URL.Query()
+	from, ok1 := params["from"]
+	to, ok2 := params["to"]
+
+	if !ok1 || !ok2 {
+		w.WriteHeader(400) // Bad Request
+		return
+	}
+	_, _ = from, to
+
+	// TODO:  send RPC call, and respond on HTTP request
+	fmt.Fprintln(w, "{}")
 }
